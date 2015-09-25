674099cbc4c2224f6c7c955261cedabc3b608139
Tessa <Tessachi33@yahoo.com>
2015-09-25 12:30:49 -0700

    Add logic for pizza and price

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,31 @@
-
+//Gets the size of the pizza, toppings array and set base price of 0
 function Pizza(size) {
 	this.size = size;
 	this.toppings = [];
 	this.price = 0;
 };
 
+//Gets the array of selected toppings
+Pizza.prototype.addTopping = function(topping) {
+	this.toppings.push(topping);
+};
 
-// $(document).ready(function() {
-//   $("form#@@@@@").submit(function(event) {
-//     //var number = parseInt($("input#number").val());
-//
-//     var result = @@@@@(@@@@@);
-//
-//     $(".answer").text(result);
-//
-//
-//     $("#result").show();
-//     event.preventDefault();
-//   });
-// });
+//Gets the price of pizza based on size and toppings array
+Pizza.prototype.getPrice = function() {
+	var toppings = ["seitan", "tofu", "potatoes"];
+	var total = 0;
+	if (this.size == "Large") {
+		total += 20;
+	} else if (this.size == "Medium") {
+		total += 15;
+	} else {
+		total += 10;
+	}
+  
+	for (var i = 0; i < this.toppings.length; i++) {
+		 {
+				total += 1;
+			}
+	}
+	return total;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,19 @@
 describe('Pizza', function() {
-  it("Gets right size of pizza", function() {
+  it("Gets right size of Pizza", function() {
   	var newPizza = new Pizza("Large");
     expect(newPizza.size).to.equal("Large");
+  });
+
+  it("Returns right array of toppings", function() {
+  	var newPizza = new Pizza("Large");
+  	newPizza.addTopping("seitan");
+  	newPizza.addTopping("tofu");
+    expect(newPizza.toppings).to.eql(["seitan", "tofu"]);
+  });
+
+  it("Returns right price of pizza", function() {
+  	var newPizza = new Pizza("Large");
+    expect(newPizza.getPrice()).to.equal(20);
   });
 
 });
